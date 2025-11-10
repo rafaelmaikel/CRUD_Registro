e26593bfd68bd41a4db9d94d33e2b858bfaf4a3e
rafaelmaikel <rafael.maikel1@gmail.com>
2025-11-10 00:19:01 -0300

    Atualiza views, models e adiciona templates interativos de cargos, departamentos e empregados

--- CRUD_Projeto/urls.py
+++ CRUD_Projeto/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+# O arquivo de URLs principal do projeto faz a ligação com o aplicativo
+# CRUD_Registro e define a rota de administração.
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('CRUD_Registro.urls')),  # raiz aponta para empregado_list
+    # Direciona a raiz do site ("/") para as URLs do aplicativo de cadastro
+    path('', include('CRUD_Registro.urls')),
 ]
