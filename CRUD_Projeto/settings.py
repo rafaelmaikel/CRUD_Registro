62e3d4c97bb507aa08e339840c73c0710a69d3d5
rafaelmaikel <rafael.maikel1@gmail.com>
2025-11-10 00:25:10 -0300

    Atualiza views, models e adiciona templates interativos de cargos, departamentos e empregados

--- CRUD_Projeto/settings.py
+++ CRUD_Projeto/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CRUD_Registro'
+    'CRUD_Registro',
+    'crispy_forms',        # <— adicione isto
+    'crispy_bootstrap5',   # <— se for usar Bootstrap 5; ou crispy_bootstrap4 para Bootstrap 4
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap5"]
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
